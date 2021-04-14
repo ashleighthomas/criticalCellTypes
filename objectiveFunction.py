da377e6adc9e487e86f6a9c731467c92a26216e2
Ashleigh Thomas <athomas98027@gmail.com>
2021-04-13 17:14:34 -0700

    cleaning

--- objectiveFunction.py
+++ objectiveFunction.py
@@ -1,63 +1,37 @@
-# python3 umiCountsRankCoexp.py  \
-# /share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/umiCounts/EndUMICounts.txt \
-# compactPolioEFCoexp.txt \
-# miniFolder/ \
-# EndCoexp.txt
-#this one at first will use original objective function, later will use chain rule
+#Ashleigh Thomas, contributor
+#Julie Chow, contributor
+#MS Thesis project for Ashleigh Thomas: Identifying Critical Cell Types for Neurodevelopmental Disorders (NDDs)
+
+#imports
 import numpy as np
 import pandas as pd
 import sys
 import random
 from random import randint
 import scipy.stats as stats
-def calcNormSum(df, geneSum, moduleGenes,comparison):#,barcode):
-	m = df.shape[0]
+#function that accepts:
+#1: a pandas DataFrame (df) that represents UMI counts per barcoded cell per gene from a single cell RNA-seq dataset.
+#2: geneSum: an float representing the coexpression of genes, currently not being used.
+#3: moduleGenes: a list of strings contaning the names of the genes in the module of interest.
+#4: comparison: a flag that if == 1 returns data that will be used to make a heatmap of Z-scored per cell type, averaged per cell type UMI data.
+def calcNormSum(df, geneSum, moduleGenes,comparison):
+	m = df.shape[0]#df dimensions
 	n = df.shape[1]
-	if geneSum == 0:
+	if geneSum == 0:#if not using coexpression data, let it be 1 (since it will be multiplied in, so this will have no effect)
 		geneSum = 1
 	rankDF = df.rank(axis=0,method='dense')#ranking column
 	#filter rankDF to only have moduleGenes
 	rankDF = rankDF[rankDF.index.isin(moduleGenes)]
-#	print(rankDF)
-#	rankDF.to_csv('M2MicRank.txt', sep='\t')
 	if comparison == 1:
-#		if barcode in rankDF.columns:
-#			rankDF[barcode].to_csv('barcode.txt',sep='\t')
 		return rankDF
-#		resultDict = rankDF.to_dict()
-#		return resultDict
-	rankDF['sum'] = rankDF.sum(axis=1)#sum of ranks
-	rankDF['normSum'] = rankDF['sum'] / (n-1)#*(maxRank-1))#removing *(maxRank-1) removes the problem of small cell types getting larger OF values
-	#normSum being rankDF['sum']/(n-1) was what the successful run was that was reported in file M1NoCoexpressionOutput021821.txt and M2NoCoexpressionOutput021821.txt. versions with coexpression (single coexpression value not made for coexpression of only module genes) can be found at: M1CoexpressionOutput021921.txt and M2CoexpressionOutput021921.txt (also in powerpoint LabUpdate022221.ppt)
-	rankDF['normLenModGenesSum'] = rankDF['normSum'].pow(1/len(moduleGenes))#chain rule!
-	#rankDF['normSum'] = rankDF['normSum'] * geneSum#this used to be nested in if useAllGenes#commenting out 02/16/21
-	normSumDF = rankDF['normLenModGenesSum']#used to be 'normSum'
-	resultDict = normSumDF.to_dict()
-	return resultDict
+	rankDF['sum'] = rankDF.sum(axis=1)#sum of ranks of human genes in genome
+	rankDF['normSum'] = rankDF['sum'] / (n-1)#summed ranks of UMI counts of all human genes divided by number of cells - 1
+	rankDF['normLenModGenesSum'] = rankDF['normSum'].pow(1/len(moduleGenes))#put the summed ranks of UMI counts/#cells-1 to the power of (1/number of module genes)
+	normSumDF = rankDF['normLenModGenesSum']#grabbing dataframe from only the column that has been exponentiated
+	resultDict = normSumDF.to_dict()#turning said dataframe into a dictionary to return
+	return resultDict#dictionary mapping from gene to value
 
-def calcNormSumNoCoexpression(df, moduleGenes, comparison):#,barcode):
-	m = df.shape[0]
-	n = df.shape[1]
-	rankDF = df.rank(axis=0,method='dense')#ranking column
-	#filter rankDF to only have moduleGenes
-	rankDF = rankDF[rankDF.index.isin(moduleGenes)]
-	if comparison == 1:
-#		if barcode in rankDF.columns:
-#			rankDF[barcode].to_csv('barcode.txt',sep='\t')
-		return rankDF
-#		resultDict = rankDF.to_dict()
-#		return resultDict
-	rankDF['sum'] = rankDF.sum(axis=1)#sum of ranks
-	rankDF['normSum'] = rankDF['sum'] / (n-1)#*(maxRank-1))#removing *(maxRank-1) removes the problem of small cell types getting larger OF values
-	#normSum being rankDF['sum']/(n-1) was what the successful run was that was reported in file M1NoCoexpressionOutput021821.txt and M2NoCoexpressionOutput021821.txt. versions with coexpression (single coexpression value not made for coexpression of only module genes) can be found at: M1CoexpressionOutput021921.txt and M2CoexpressionOutput021921.txt (also in powerpoint LabUpdate022221.ppt)
-	rankDF['normLenModGenesSum'] = rankDF['normSum'].pow(1/len(moduleGenes))#chain rule!
-	#rankDF['normSum'] = rankDF['normSum'] * geneSum#this used to be nested in if useAllGenes#commenting out 02/16/21
-	normSumDF = rankDF['normLenModGenesSum']#used to be 'normSum'
-	resultDict = normSumDF.to_dict()
-	return resultDict
-
-def leftHandSide(genes, umiDF, co_exp, comparison):#,barcode):	
-#def leftHandSide(genes, umiDF, comparison):#,barcode):	
+def leftHandSide(genes, umiDF, co_exp, comparison):
 	co_exp_dictionary = {}
 	for gene in reversed(genes):
 		idx = genes.index(gene)
@@ -84,42 +58,9 @@ def leftHandSide(genes, umiDF, co_exp, comparison):#,barcode):
 	geneSum = geneSumDF[0].sum(axis=0)
 	geneSumDF.columns=['value']
 	#now to use coexpression and take into account all ~24k genes into the ranking
-	lhsDict = calcNormSum(umiDF, geneSum, genes,comparison)#,barcode)#this is multiplying coexpression sum of module genes by umi counts for all genes
+	lhsDict = calcNormSum(umiDF, geneSum, genes,comparison)#this is multiplying coexpression sum of module genes by umi counts for all genes
 	return lhsDict, geneSum
 
-def leftHandSideWithoutCoexpression(genes, umiDF, comparison):#,barcode):	
-#def leftHandSide(genes, umiDF, comparison):#,barcode):	
-	'''
-	co_exp_dictionary = {}
-	for gene in reversed(genes):
-		idx = genes.index(gene)
-		spec_co_exp = co_exp[(co_exp['geneA'] == gene) | (co_exp['geneB'] == gene)]
-		growing_co_exp = 0  # growing summation
-		for chain_idx in np.arange(0, idx):
-			gene_a = genes[idx]
-			gene_b = genes[chain_idx]
-			try_c_1 = spec_co_exp[(spec_co_exp['geneA'] == gene_a) & (spec_co_exp['geneB'] == gene_b)]
-			try_c_2 = spec_co_exp[(spec_co_exp['geneB'] == gene_a) & (spec_co_exp['geneA'] == gene_b)]
-			if try_c_1.empty and try_c_2.empty:  # Then the co-expression doesn't exist, set to 0
-				c_value = 0
-			elif try_c_2.empty:
-				c_value = try_c_1.iloc[0]['coexp']
-			else:
-				c_value = try_c_2.iloc[0]['coexp']
-
-			growing_co_exp += c_value
-		if growing_co_exp == 0:  # if there are no co-expression values associated, just set to 1. will do nothing
-			growing_co_exp = 1
-		co_exp_dictionary[gene] = growing_co_exp
-	geneSumDF = pd.DataFrame.from_dict(co_exp_dictionary, orient='index')
-	geneSumDF = geneSumDF.dropna()
-	geneSum = geneSumDF[0].sum(axis=0)
-	geneSumDF.columns=['value']
-	'''
-	#now to use coexpression and take into account all ~24k genes into the ranking
-#	lhsDict = calcNormSum(umiDF, geneSum, genes,comparison)#,barcode)#this is multiplying coexpression sum of module genes by umi counts for all genes
-	lhsDict = calcNormSumNoCoexpression(umiDF, genes, comparison)
-	return lhsDict#, geneSum
 def rightHandSide(distMatrix, cellTypeInterest):
 	cellMeta = open('/share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/cell_metadata.csv')
 	typeBarcodeDict = {}
@@ -316,119 +257,28 @@ def rightHandSidePValue(btwnDF, withinDF):
 
 #function calls:
 moduleGenes = getModuleGenes()
-print('got module genes, about to get coexpression data for module genes')
 fileModifier = sys.argv[6]
 comparison = float(sys.argv[7])
 coexpDF = getCoexpressionDataChunks(moduleGenes, fileModifier)#TODO uncomment this for later
-print('got coexpression data')
 umiFilesList = getUMIFiles()#TODO uncomment when done with random barcodes
-#umiFilePath = sys.argv[3]
-#barcodes = chooseRandomBarcodes(9995, 'allBarcodes.txt')
-#print(len(barcodes))
-#umiFile = open(sys.argv[3])
-#umiFilesList = []
-#umiFilesList.append(umiFile)
-#print(barcodes)
-#print(umiFilesList)#['/share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/umiCounts/ExNUMICounts.txt', '/share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/umiCounts/MicUMICounts.txt']
-#exit()
 outfile = open(sys.argv[5], 'w')
 outfile.write('Gene\tLog\tRaw\n')
-#outfile = open('outputRandomM2Ext_021221.txt', 'w')
-barcode = 'ACTGGCACCGAG'
-#primeUniversePerCellTypeDict = getPrimeUniverse(umiFilesList)
-#print(primeUniversePerCellTypeDict['ExN'])
-#exit()
-cellType = 'allCellsRandomSubset'
-#print(umiFilePath)
-#exit()
-#umiDF = getUMICount(umiFilePath)
-#print(umiDF)
-'''
-print('about to call getDistMatrix,***')
-btwnMatrix, withinMatrix = getDistanceMatrix(barcodes)#note, this is currently the prime distance matrix
-#exit()
-print(btwnMatrix.shape)
-print(withinMatrix.shape)
-#	np.fill_diagonal(distMatrix.values, 0)#NOTE this is for when the matrix is square (aka when we are getting only barcodes for this cell type, uncomment this later)
-np.fill_diagonal(withinMatrix.values, 0)#NOTE this is for when the matrix is square (aka when we are getting only barcodes for this cell type, uncomment this later)
-df = pd.DataFrame()
-df["max_sim_per_cell"] = btwnMatrix.max(axis=0)#axis=1 is avg of row (previously), now doing avg of col#TODO change to distMatrix.max
-df['max_sim_per_cell'].to_csv("%s_max_sim_per_cell_with_cells_not_in_same_cell_type.txt" % cellType)#val per cell
-summaryVal = df['max_sim_per_cell'].sum() / df.shape[0]#this now represents the average similarity with cells outside of the cell type per cell type
-sumFile = open(cellType + 'BetweenSummaryValue.txt', 'w')
-sumFile.write(str(summaryVal))
-sumFile.close()
-df = pd.DataFrame()
-df["max_sim_per_cell"] = withinMatrix.max(axis=0)#axis=1 is avg of row (previously), now doing avg of col#TODO change to distMatrix.max
-df['max_sim_per_cell'].to_csv("%s_max_sim_per_cell_within_barcode_group.txt" % cellType)#val per cell
-summaryVal = df['max_sim_per_cell'].sum() / df.shape[0]#this now represents the average similarity with cells outside of the cell type per cell type
-sumFile = open(cellType + 'WithinSummaryValue.txt', 'w')
-sumFile.write(str(summaryVal))
-sumFile.close()
-exit()
-'''
-#rhsOutfile = open('rightHandSidePValuesResults033021.txt', 'w')
 for cellTypeFile in umiFilesList:
-	line = cellTypeFile.split('/')#TODO uncomment these next 3 lines after done with random barcodese xperiment
+	line = cellTypeFile.split('/')
 	cellType = line[-1]
 	cellType = cellType[:-13]#remove the UMICounts.txt from the end (i.e. EndUMICounts.txt becomes cell type of End)
-#	cellType = 'allCellsRandomSubset'#TODO remove this after the random barcodes experiment is done
 	print('cell type:', cellType)
-	print('about to open file', cellTypeFile)
 	umiDF = getUMICount(cellTypeFile)
-	print(umiDF)
-	barcodes = umiDF.columns#TODO uncomment this after random barcodes experiment done
-	print('got UMI count')
-	#distMatrix = getDistanceMatrix(barcodes)#note, this is currently the prime distance matrix
-	btwnMatrix, withinMatrix = getDistanceMatrix(barcodes)#note, this is currently the prime distance matrix
+	barcodes = umiDF.columns
+	btwnMatrix, withinMatrix = getDistanceMatrix(barcodes)#note withinMatrix is the prime distance matrix, btwnMatrix is the matrix of comparisons amongst the same cell type
 	rhsVal = rightHandSidePValue(btwnMatrix, withinMatrix)
-#	rhsOutfile.write(cellType + '\t' + str(rhsVal) + '\n')
-	#rhsOutfile.close()
-	#exit()
-#	np.fill_diagonal(distMatrix.values, 0)#NOTE this is for when the matrix is square (aka when we are getting only barcodes for this cell type, uncomment this later)
-	#this next section of code was implemented, now commented out to see what rightHandSidePValue returns (does same thing except 1-pval)
-#	np.fill_diagonal(withinMatrix.values, 0)#NOTE this is for when the matrix is square (aka when we are getting only barcodes for this cell type, uncomment this later)
-#	df = pd.DataFrame()
-#	df["max_sim_per_cell"] = btwnMatrix.max(axis=0)#axis=1 is avg of row (previously), now doing avg of col#TODO change to distMatrix.max
-#	df['max_sim_per_cell'].to_csv("%s_max_sim_per_cell_with_cells_not_in_same_cell_type032921.txt" % cellType)#val per cell
-#	summaryVal = df['max_sim_per_cell'].sum() / df.shape[0]#this now represents the average similarity with cells outside of the cell type per cell type
-#	sumFile = open(cellType + 'BetweenSummaryValue032921.txt', 'w')
-#	sumFile.write(str(summaryVal))
-#	sumFile.close()
-
-#	df = pd.DataFrame()
-#	df["max_sim_per_cell"] = withinMatrix.max(axis=0)#axis=1 is avg of row (previously), now doing avg of col#TODO change to distMatrix.max
-#	df['max_sim_per_cell'].to_csv("%s_max_sim_per_cell_within_barcode_group032921.txt" % cellType)#val per cell
-#	summaryVal = df['max_sim_per_cell'].sum() / df.shape[0]#this now represents the average similarity with cells outside of the cell type per cell #type
-#	sumFile = open(cellType + 'WithinSummaryValue032921.txt', 'w')
-#	sumFile.write(str(summaryVal))
-#	sumFile.close()
-#	exit()
-	#distMatrix['max_sim_per_cell'].to_csv("%s_max_sim_per_cell.txt" % fileModifier)
-#	exit()
-	print('got distance matrix')
-	print('about to call left hand side func')
-	lhsDict, geneSum = leftHandSide(moduleGenes, umiDF, coexpDF,comparison)#,barcode)
-#	lhsDict = leftHandSideWithoutCoexpression(moduleGenes, umiDF, comparison)# coexpDF,comparison)#,barcode)
-	print('got lhs values')
-	#rhsVal = rightHandSide(distMatrix, cellType)#TODO uncomment once I'm done running the prime universe calculations
-	print('got rhs values')
-	print('---------------------------------')
-	#objectiveFuncValueLog, rawObjectiveFunctionValue = multiplication(lhsDict, rhsVal, geneSum,comparison, fileModifier, cellType)#with coexpression
+	lhsDict, geneSum = leftHandSide(moduleGenes, umiDF, coexpDF,comparison)
 	objectiveFuncValueLog, rawObjectiveFunctionValue = multiplication(lhsDict, rhsVal, comparison, fileModifier, cellType)#without coexpression
-#	print('finished multiplication, now writing to output file, final value was', objectiveFuncValueLog, rawObjectiveFunctionValue)
 	if comparison != 1:
 		outfile.write(cellType + '\t' + str(objectiveFuncValueLog) + '\t' + str(rawObjectiveFunctionValue) + '\n')
 	else:
-#		dfZScored = objectiveFuncValueLog.mean(axis=1)
-		#dfZScored.to_csv(fileModifier +cellType+ '.txt', sep='\t', header=False)
 		objectiveFuncValueLog.to_csv(fileModifier +cellType+ '.txt', sep='\t')
-		
-		
 outfile.close()
-#rhsOutfile.close()
-#outfile.close()
-#rhsOutfile.close()
 #example of how to run for End cell type
 #python3 objectiveFunctionChainRuleAllCellTypesNonMaxRank_max_sim.py m2ExtendedASDwithID.txt polioEFCoexpressionDataWithoutNans.txt /share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/umiCounts/EndUMICounts.txt /share/hormozdiarilab/Data/scRNASeq_AllenBrain/RNN_snn.csv End
 #python3 objectiveFunctionChainRuleAllCellTypesNonMaxRank_max_sim.py m2ExtendedASDwithID.txt polioEFCoexpressionDataNoNanNoZero.txt /share/hormozdiarilab/Experiments/Ashleigh_workspace/nddTissue/forJulie/umiCounts/EndUMICounts.txt /share/hormozdiarilab/Data/scRNASeq_AllenBrain/RNN_snn.csv End
